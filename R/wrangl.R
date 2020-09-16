c1a1329c857b3b9d3e4e31deaf442af551b1d851
andrew <arenn@design.upenn.edu>
2020-09-15 22:32:29 -0400

    Exploration

--- R/wrangl.R
+++ R/wrangl.R
@@ -18,11 +18,60 @@ files <- files[3:7]
 
 poi <- map_df(files, vroom)
 
+glimpse(poi)
+
 ##
 
-poi <- lazy_dt(poi)
-poi <- poi %>% 
+phl <- 
+  poi %>% 
+  lazy_dt() %>%
   filter(city == "Philadelphia" & region == "PA") %>%
   as_tibble()
 
+## 
+
+reg <- 
+  counties(cb = TRUE, class = 'sf') %>%
+  filter(str_detect(STATEFP, "42|34|10|24")) %>%
+  filter(str_detect(NAME, "Bucks|Chester|Delaware|Montgomery|Philadelphia|Burlington|Camden|Gloucester|New Castle|Salem|Cecil")) %>%
+  filter(GEOID != "24031")
+
 ##
+
+met <- 
+  poi %>% 
+  lazy_dt() %>%
+  filter(str_detect(region, "PA|NJ|DE")) %>%
+  as_tibble() %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+  st_transform(4269) %>%
+  st_intersection(reg)
+
+##
+
+met %>% 
+  st_drop_geometry() %>%
+  group_by(top_category) %>% 
+  summarise(n = n()) %>%
+  ungroup() %>%
+  arrange(desc(n)) %>%
+  slice(1:10) %>%
+  rename(category = top_category) %>%
+  knitr::kable()
+
+##
+
+cbg <- 
+  reduce(
+    map(unique(reg$STATEFP), function(x){
+      block_groups(x, cb = TRUE, class = 'sf')
+    }),
+    rbind
+  ) %>%
+  filter(str_sub(GEOID, 1, 5) %in% reg$GEOID)
+
+##
+
+
+    
+
